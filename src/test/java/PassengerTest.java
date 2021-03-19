b9bb0f1c46f4beb063d4b77af26b36986adccb77
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-19 11:44:53 +0000

    set up Passenger class & test files

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Rachel", 1);
+    }
+}
+
